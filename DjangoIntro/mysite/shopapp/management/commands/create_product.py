f7a561d850af35dfd380c74e88e137c8bebd83e4
Miron-Anosov <mairon26rus@gmail.com>
2024-10-27 01:36:18 +0200

    feat: add new fields

--- DjangoIntro/mysite/shopapp/management/commands/create_product.py
+++ DjangoIntro/mysite/shopapp/management/commands/create_product.py
@@ -5,27 +5,37 @@ from shopapp.models import Product
 class Command(BaseCommand):
     """Create product."""
     help = 'Creates products information.'
+
     def handle(self, *args, **options):
         self.stdout.write("Product is creating.")
         data_prod = {
             "prod_1": {
                 "name": "Phones",
-        },
-           "product_2": {
+                "discount": 15,
+                "reviews": 3,
+                "price": 100
+            },
+            "product_2": {
                 "name": "Laptops",
-                "description": "Product is well."
+                "description": "Product is well.",
+                "discount": 5,
+                "reviews": 45,
+                "price": 150
             },
 
         }
 
         for user in data_prod.values():
-            prod, error = Product.objects.get_or_create(
+            prod, if_exist = Product.objects.get_or_create(
                 name=user.get("name"),
                 description=user.get("description"),
+                discount=user.get("discount"),
+                reviews=user.get("reviews"),
+                price=user.get("price")
             )
 
-            if error:
-                self.stdout.write(self.style.ERROR("Could not create!"))
+            if not if_exist:
+                self.stdout.write(self.style.ERROR(f"{prod} created!"))
             else:
                 self.stdout.write(self.style.SUCCESS(prod))
                 self.stdout.write(self.style.SUCCESS('Successfully created a new user!'))
